eb69cc842eb349e84c06427ca942c6190d2f2b36
= <=>
2020-03-14 21:32:26 +0100

    feat: add slider

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ navigation.addEventListener('click', (event) => {
 });
 
 button.addEventListener('click', (event) => {
-
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
 
@@ -26,10 +25,9 @@ button.addEventListener('click', (event) => {
   }
 
   const descriptionText = !(describe === '') ? `Describe: ${describe}` : 'Without Description';
+
   document.getElementById('message-describe').innerText = descriptionText;
-
   document.getElementById('message-block').classList.remove('hidden');
-
   event.preventDefault();
 });
 
@@ -39,50 +37,27 @@ closeButton.addEventListener('click', () => {
   document.getElementById('contact-form').reset();
 });
 
-
-
-
-//
-// var slides = document.querySelectorAll('.slider__item');
-// var currentSlide = 0;
-// var slideInterval = setInterval(nextSlide,2000);
-//
-// function nextSlide() {
-//   slides[currentSlide].className = 'slider__item';
-//   currentSlide = (currentSlide+1)%slides.length;
-//   slides[currentSlide].className = 'show-slide';
-// }
 //-------Переключение слайдов-------------------
 
-// const leftArrow = document.getElementsByClassName('slider__control_left');
-// const rightArrow = document.getElementsByClassName('slider__control_right');
-//
-//
-//
-//
-// leftArrow.addEventListener('click', () => {
-//
-//   const slides = document.querySelectorAll('.slider__item');
-//   console.log('slides=', slides);
-//   let currentSlide = 0;
-//   slides[currentSlide].className = '.slider__item';
-//   currentSlide = (currentSlide+1)%slides.length;
-//   // document.getElementsByClassName('slider__item').classList.add('show-slide');
-//
-//   slides[currentSlide].className = '.slider__item show-slide'
-//
-// });
+const leftArrow = document.querySelector('.slider__control_left');
+const rightArrow = document.querySelector('.slider__control_right');
+const sliderRed = document.querySelector('.slider__item-red');
+const sliderBlue = document.querySelector('.slider__item-blue');
+
+const toggleSlide = () => {
+  sliderRed.classList.toggle('show-slide');
+  sliderBlue.classList.toggle('show-slide');
+};
+
+leftArrow.addEventListener('click', toggleSlide);
+rightArrow.addEventListener('click', toggleSlide);
 
 //--------Активация экранов телефонов----------------------------
 
 const horizontalPhone = document.getElementsByClassName('phone__horizontal_img')[0];
 const horizontalScreen = document.getElementsByClassName('horizontal-screen')[0];
-
 const verticalPhone = document.getElementsByClassName('phone__vertical_img')[0];
 const verticalScreen = document.getElementsByClassName('vertical-screen')[0];
-
-
-
 
 horizontalPhone.addEventListener('click', (event) => {
   document.getElementsByClassName('horizontal-screen')[0].classList.toggle('hidden-screen');
@@ -93,19 +68,16 @@ horizontalScreen.addEventListener('click', (event) => {
 });
 
 verticalPhone.addEventListener('click', () => {
-  console.log('hello000');
-
   document.getElementsByClassName('vertical-screen')[0].classList.toggle('hidden-screen');
 });
 
 verticalScreen.addEventListener('click', () => {
-  console.log('hello000');
-
   document.getElementsByClassName('vertical-screen')[0].classList.toggle('hidden-screen');
 });
 
 
 //------Portfolio. Переключение табов------------
+
 const gallery = document.getElementById('gallery');
 const allButton = document.getElementById('all');
 const webButton = document.getElementById('web');
@@ -123,8 +95,7 @@ const allCards = [
   {path: 'assets/portfolio/09.png', name: 'Image09'},
   {path: 'assets/portfolio/10.png', name: 'Image10'},
   {path: 'assets/portfolio/11.png', name: 'Image11'},
-  {path: 'assets/portfolio/12.png', name: 'Image12'},
-
+  {path: 'assets/portfolio/12.png', name: 'Image12'}
 ];
 
 const webCards = [
@@ -173,106 +144,33 @@ const graphicCards = [
 ];
 const filter = document.getElementById('filter');
 
-
 allButton.addEventListener('click', () => showCards(allCards));
 webButton.addEventListener('click', () => showCards(webCards));
 artButton.addEventListener('click', () => showCards(artCards));
 graphicButton.addEventListener('click', () => showCards(graphicCards));
 
-
 function showCards(arr) {
   let cards = '';
-
   arr.forEach(el => {
     cards += `<div class="portfolio__column"><img src="${el.path}" alt="${el.name}" width="220" height="187"></div>`
   });
-
   gallery.innerHTML = cards;
 }
-
-
 
 filter.addEventListener('click', (event) => {
   filter.querySelectorAll('.filter__button')
     .forEach(el => el.classList.remove('active'));
   event.target.classList.add('active');
-
 });
-
-
 
 //------Portfolio. Взаимодействие с картинками------------
 
 gallery.addEventListener('click', (event) => {
   gallery.querySelectorAll('.portfolio__column')
     .forEach(el => {
-      console.log(el);
       el.firstChild.classList.remove('portfolio__column_bordered')
     });
   event.target.classList.add('portfolio__column_bordered');
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function Sim(sldrId) {
-//
-//   let id = document.getElementById(sldrId);
-//   if(id) {
-//     this.sldrRoot = id
-//   }
-//   else {
-//     this.sldrRoot = document.querySelector('.slider')
-//   };
-//
-//   // Carousel objects
-//   this.sldrList = this.sldrRoot.querySelector('.slider__content');
-//   this.sldrElements = this.sldrList.querySelectorAll('.slider__item');
-//   this.sldrElemFirst = this.sldrList.querySelector('.slider__item');
-//   this.leftArrow = this.sldrRoot.querySelector('slider__control_left');
-//   this.rightArrow = this.sldrRoot.querySelector('slider__control_right');
-//
-//   // Initialization
-//   this.options = Sim.defaults;
-//   Sim.initialize(this)
-// };
-//
-// Sim.defaults = {
-//
-//   // Default options for the carousel
-//   loop: true,     // Бесконечное зацикливание слайдера
-//   arrows: true,   // Пролистывание стрелками
-// };
-//
-// Sim.prototype.elemPrev = function(num) {
-//   num = num || 1;
-//
-//   let prevElement = this.currentElement;
-//   this.currentElement -= num;
-//   if(this.currentElement < 0) this.currentElement = this.elemCount-1;
-//
-//   if(!this.options.loop) {
-//     if(this.currentElement === 0) {
-//       this.leftArrow.style.display = 'none'
-//     };
-//     this.rightArrow.style.display = 'block'
-//   };
-//
-//   this.sldrElements[this.currentElement].style.opacity = '1';
-//   this.sldrElements[prevElement].style.opacity = '0';
-//
-// };
-
 showCards(allCards);
-
-
-
