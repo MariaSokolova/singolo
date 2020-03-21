438127315b6ce7a6385433f6176f2968202efcf1
= <=>
2020-03-21 21:12:38 +0100

    feat: add smooth scrolling to slider

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 //-------Scroll-------------------
 
 const navigation = document.getElementById('navigation');
-
-
 let avgHeight = 0;
 const sections = document.querySelectorAll('section');
+
 sections.forEach(el => {
   avgHeight += el.offsetHeight;
 });
+
 let ScrollOffset = document.documentElement.clientHeight - parseInt(avgHeight / sections.length);
 ScrollOffset = (ScrollOffset < 30) ? document.querySelector('header').offsetHeight : ScrollOffset;
-
 
 document.addEventListener('scroll', onScroll);
 
@@ -20,7 +19,7 @@ function onScroll(event) {
   const links = document.querySelectorAll('#navigation a');
 
   divs.forEach((el) => {
-    if(el.offsetTop <= currentPosition && (el.offsetTop + el.offsetHeight - 60) > currentPosition) {
+    if (el.offsetTop <= currentPosition && (el.offsetTop + el.offsetHeight - 60) > currentPosition) {
       links.forEach((a) => {
         a.classList.remove(('navigation__active'));
         if (el.getAttribute('id') === a.getAttribute('href').substring(1)) {
@@ -34,16 +33,12 @@ function onScroll(event) {
     navigation.querySelector('a.navigation__active').classList.remove('navigation__active');
     links[links.length - 1].classList.add('navigation__active');
   }
-  // if (navigation.querySelector('a.navigation__active') === null) {
-  //   links[0].classList.add('navigation__active');
-  // }
 }
+
+//-------Form, Modal window-------------------
 
 const button = document.getElementById('button');
 const closeButton = document.getElementById('close-btn');
-
-
-//-------Form, Modal window-------------------
 
 button.addEventListener('click', (event) => {
   const name = document.getElementById('name').value;
@@ -68,7 +63,6 @@ button.addEventListener('click', (event) => {
   event.preventDefault();
 });
 
-
 closeButton.addEventListener('click', () => {
   document.getElementById('message-block').classList.add('hidden');
   document.getElementById('contact-form').reset();
@@ -76,18 +70,61 @@ closeButton.addEventListener('click', () => {
 
 //-------Переключение слайдов-------------------
 
+let items = document.querySelectorAll('.slider__item');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+  currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+  isEnabled = false;
+  items[currentItem].classList.add(direction);
+  items[currentItem].addEventListener('animationend', function () {
+    this.classList.remove('slider__active', direction);
+  });
+}
+
+function showItem(direction) {
+  items[currentItem].classList.add('next', direction);
+  items[currentItem].addEventListener('animationend', function () {
+    this.classList.remove('next', direction);
+    this.classList.add('slider__active');
+    isEnabled = true;
+  });
+}
+
+function nextItem(n) {
+  console.log('nextItem', n);
+  hideItem('to-left');
+  changeCurrentItem(n + 1);
+  showItem('from-right');
+}
+
+function previousItem(n) {
+  console.log('previousItem', n);
+  hideItem('to-right');
+  changeCurrentItem(n - 1);
+  showItem('from-left');
+}
+
 const leftArrow = document.querySelector('.slider__control_left');
 const rightArrow = document.querySelector('.slider__control_right');
-const sliderRed = document.querySelector('.slider__item-red');
-const sliderBlue = document.querySelector('.slider__item-blue');
 
-const toggleSlide = () => {
-  sliderRed.classList.toggle('show-slide');
-  sliderBlue.classList.toggle('show-slide');
-};
+leftArrow.addEventListener('click', function () {
+  console.log('leftArrow');
+  if (isEnabled) {
+    previousItem(currentItem);
+  }
+});
 
-leftArrow.addEventListener('click', toggleSlide);
-rightArrow.addEventListener('click', toggleSlide);
+rightArrow.addEventListener('click', function () {
+  console.log('rightArrow');
+  if (isEnabled) {
+    nextItem(currentItem);
+  }
+});
 
 //--------Активация экранов телефонов----------------------------
 
@@ -121,63 +158,63 @@ const webButton = document.getElementById('web');
 const artButton = document.getElementById('art');
 const graphicButton = document.getElementById('graphic');
 const allCards = [
-  {path: 'assets/portfolio/01.png', name: 'Image01'},
-  {path: 'assets/portfolio/02.png', name: 'Image02'},
-  {path: 'assets/portfolio/03.png', name: 'Image03'},
-  {path: 'assets/portfolio/04.png', name: 'Image04'},
-  {path: 'assets/portfolio/05.png', name: 'Image05'},
-  {path: 'assets/portfolio/06.png', name: 'Image06'},
-  {path: 'assets/portfolio/07.png', name: 'Image07'},
-  {path: 'assets/portfolio/08.png', name: 'Image08'},
-  {path: 'assets/portfolio/09.png', name: 'Image09'},
-  {path: 'assets/portfolio/10.png', name: 'Image10'},
-  {path: 'assets/portfolio/11.png', name: 'Image11'},
-  {path: 'assets/portfolio/12.png', name: 'Image12'}
+  { path: 'assets/portfolio/01.png', name: 'Image01' },
+  { path: 'assets/portfolio/02.png', name: 'Image02' },
+  { path: 'assets/portfolio/03.png', name: 'Image03' },
+  { path: 'assets/portfolio/04.png', name: 'Image04' },
+  { path: 'assets/portfolio/05.png', name: 'Image05' },
+  { path: 'assets/portfolio/06.png', name: 'Image06' },
+  { path: 'assets/portfolio/07.png', name: 'Image07' },
+  { path: 'assets/portfolio/08.png', name: 'Image08' },
+  { path: 'assets/portfolio/09.png', name: 'Image09' },
+  { path: 'assets/portfolio/10.png', name: 'Image10' },
+  { path: 'assets/portfolio/11.png', name: 'Image11' },
+  { path: 'assets/portfolio/12.png', name: 'Image12' }
 ];
 
 const webCards = [
-  {path: 'assets/portfolio/03.png', name: 'Image03'},
-  {path: 'assets/portfolio/04.png', name: 'Image04'},
-  {path: 'assets/portfolio/05.png', name: 'Image05'},
-  {path: 'assets/portfolio/06.png', name: 'Image06'},
-  {path: 'assets/portfolio/07.png', name: 'Image07'},
-  {path: 'assets/portfolio/08.png', name: 'Image08'},
-  {path: 'assets/portfolio/09.png', name: 'Image09'},
-  {path: 'assets/portfolio/10.png', name: 'Image10'},
-  {path: 'assets/portfolio/11.png', name: 'Image11'},
-  {path: 'assets/portfolio/12.png', name: 'Image12'},
-  {path: 'assets/portfolio/01.png', name: 'Image01'},
-  {path: 'assets/portfolio/02.png', name: 'Image02'}
+  { path: 'assets/portfolio/03.png', name: 'Image03' },
+  { path: 'assets/portfolio/04.png', name: 'Image04' },
+  { path: 'assets/portfolio/05.png', name: 'Image05' },
+  { path: 'assets/portfolio/06.png', name: 'Image06' },
+  { path: 'assets/portfolio/07.png', name: 'Image07' },
+  { path: 'assets/portfolio/08.png', name: 'Image08' },
+  { path: 'assets/portfolio/09.png', name: 'Image09' },
+  { path: 'assets/portfolio/10.png', name: 'Image10' },
+  { path: 'assets/portfolio/11.png', name: 'Image11' },
+  { path: 'assets/portfolio/12.png', name: 'Image12' },
+  { path: 'assets/portfolio/01.png', name: 'Image01' },
+  { path: 'assets/portfolio/02.png', name: 'Image02' }
 ];
 
 const artCards = [
-  {path: 'assets/portfolio/05.png', name: 'Image05'},
-  {path: 'assets/portfolio/06.png', name: 'Image06'},
-  {path: 'assets/portfolio/07.png', name: 'Image07'},
-  {path: 'assets/portfolio/08.png', name: 'Image08'},
-  {path: 'assets/portfolio/09.png', name: 'Image09'},
-  {path: 'assets/portfolio/10.png', name: 'Image10'},
-  {path: 'assets/portfolio/11.png', name: 'Image11'},
-  {path: 'assets/portfolio/12.png', name: 'Image12'},
-  {path: 'assets/portfolio/01.png', name: 'Image01'},
-  {path: 'assets/portfolio/02.png', name: 'Image02'},
-  {path: 'assets/portfolio/03.png', name: 'Image03'},
-  {path: 'assets/portfolio/04.png', name: 'Image04'}
+  { path: 'assets/portfolio/05.png', name: 'Image05' },
+  { path: 'assets/portfolio/06.png', name: 'Image06' },
+  { path: 'assets/portfolio/07.png', name: 'Image07' },
+  { path: 'assets/portfolio/08.png', name: 'Image08' },
+  { path: 'assets/portfolio/09.png', name: 'Image09' },
+  { path: 'assets/portfolio/10.png', name: 'Image10' },
+  { path: 'assets/portfolio/11.png', name: 'Image11' },
+  { path: 'assets/portfolio/12.png', name: 'Image12' },
+  { path: 'assets/portfolio/01.png', name: 'Image01' },
+  { path: 'assets/portfolio/02.png', name: 'Image02' },
+  { path: 'assets/portfolio/03.png', name: 'Image03' },
+  { path: 'assets/portfolio/04.png', name: 'Image04' }
 ];
 
 const graphicCards = [
-  {path: 'assets/portfolio/07.png', name: 'Image07'},
-  {path: 'assets/portfolio/08.png', name: 'Image08'},
-  {path: 'assets/portfolio/09.png', name: 'Image09'},
-  {path: 'assets/portfolio/10.png', name: 'Image10'},
-  {path: 'assets/portfolio/11.png', name: 'Image11'},
-  {path: 'assets/portfolio/12.png', name: 'Image12'},
-  {path: 'assets/portfolio/01.png', name: 'Image01'},
-  {path: 'assets/portfolio/02.png', name: 'Image02'},
-  {path: 'assets/portfolio/03.png', name: 'Image03'},
-  {path: 'assets/portfolio/04.png', name: 'Image04'},
-  {path: 'assets/portfolio/05.png', name: 'Image05'},
-  {path: 'assets/portfolio/06.png', name: 'Image06'}
+  { path: 'assets/portfolio/07.png', name: 'Image07' },
+  { path: 'assets/portfolio/08.png', name: 'Image08' },
+  { path: 'assets/portfolio/09.png', name: 'Image09' },
+  { path: 'assets/portfolio/10.png', name: 'Image10' },
+  { path: 'assets/portfolio/11.png', name: 'Image11' },
+  { path: 'assets/portfolio/12.png', name: 'Image12' },
+  { path: 'assets/portfolio/01.png', name: 'Image01' },
+  { path: 'assets/portfolio/02.png', name: 'Image02' },
+  { path: 'assets/portfolio/03.png', name: 'Image03' },
+  { path: 'assets/portfolio/04.png', name: 'Image04' },
+  { path: 'assets/portfolio/05.png', name: 'Image05' },
+  { path: 'assets/portfolio/06.png', name: 'Image06' }
 ];
 const filter = document.getElementById('filter');
 
